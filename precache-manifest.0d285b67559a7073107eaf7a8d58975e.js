23a6d9e757a0723d4fdbdcf46bb5bcaa5a387c00
Benji <benjamin-fuertschATweb.de>
2020-04-12 20:27:11 +0200

    Updates

---
+++ precache-manifest.0d285b67559a7073107eaf7a8d58975e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c427328fb234942c2d9404b01775c0f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6df4b96740626e62adc2",
+    "url": "/monsters-rolodex/static/css/main.334a15b1.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6df4b96740626e62adc2",
+    "url": "/monsters-rolodex/static/js/main.f569088c.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
